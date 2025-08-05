2bd2effd40b2e148f3bf6da0d2ebdd8a9ce2ef3f
Nikolay <filyonof@gmail.com>
2025-08-05 17:29:23 +0300

    создал алгоритм случайных бросков компьютером

---
+++ project.js
@@ -0,0 +1,15 @@
+function getComputerChoice(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+const choice = getComputerChoice(3);
+let playPC;
+
+if (choice === 0) {
+    playPC = "rock";
+} else if (choice === 1) {
+    playPC = "paper";
+} else { 
+    playPC = "scissors";
+}
+console.log(playPC);
